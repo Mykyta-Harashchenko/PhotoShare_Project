ea5fc707bf60f1373972f817cef55ee38a5034c0
BogdanKozlov35 <bkozlov35@gmail.com>
2024-08-26 14:01:21 +0300

    post-comments first commit

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import sys
 import os
 import uvicorn
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'Project'))
 
 if __name__ == "__main__":
-    uvicorn.run("src.main:app", host="0.0.0.0", port=8888, reload=True)
+    uvicorn.run("Project.main:app", host="0.0.0.0", port=8888, reload=True)
